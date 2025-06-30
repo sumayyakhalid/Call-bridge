eb2837453ee8874c52a64eac438ad0d0e8e87dfc
Basil Jamil <118303446+basiljamil12@users.noreply.github.com>
2025-06-30 15:14:41 +0500

    Change /twiml route to POST method
    Updated the /twiml endpoint from GET to POST to match expected request method for callController.getTwiml.

--- backend/routes/callRoutes.js
+++ backend/routes/callRoutes.js
@@ -5,6 +5,6 @@ import * as callController from "../controllers/callController.js";
 const router = Router();
 
 router.post("/connect", callController.getCall);
-router.get("/twiml", callController.getTwiml);
+router.post("/twiml", callController.getTwiml);
 
 export default router;
